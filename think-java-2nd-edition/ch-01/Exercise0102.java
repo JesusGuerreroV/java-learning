875658140ba42dc83fea387a63d69a6fa45ac124
JesusGuerreroV <jesusvargasgro@gmail.com>
2022-11-07 00:11:39 -0600

    agregado ch-01

---
+++ think-java-2nd-edition/ch-01/Exercise0102.java
@@ -0,0 +1,15 @@
+public class Exercise0102 {
+    /*
+     * 1. Type in the Hello World program; then compile and run it.
+     * 2. Add a print statement that displays a second message after the Hello, World!.
+     * Say something witty like, “How are you?” Compile and run the program again.
+     * 3. Add a comment to the program (anywhere), recompile, and run it again. The
+     * new comment should not affect the result.
+     */
+    public static void main(String[] args) {
+        // says hello to the world
+        System.out.println("Hello, World!");
+
+        System.out.println("How are you?"); // fine, and you?
+    }
+}
